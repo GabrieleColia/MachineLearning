d727e8d8c51cf11c52359fac576db8bff24447e6
Gabriele <gabrielecolia@yahoo.it>
2018-05-15 14:58:02 +0200

    Creazione img scala di grigi e istogramma

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from matplotlib import pyplot as plt
 import argparse
 import cv2
 from PIL import Image
-#im = Image.open("p.jpg")
+
 im= cv2.imread('grant.jpg')
-#cv2.imshow("image",im)
-#cv2.waitKey(0)
 
 grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray", grey)
