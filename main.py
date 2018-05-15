a0b5b72f64ab66c379d0fc9c7482930d285711df
Gabriele <gabrielecolia@yahoo.it>
2018-05-15 14:57:04 +0200

    Ciao

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("ciao")
-print("a")
-print("b")
+
+import numpy
+from matplotlib import pyplot as plt
+import argparse
+import cv2
+from PIL import Image
+#im = Image.open("p.jpg")
+im= cv2.imread('grant.jpg')
+#cv2.imshow("image",im)
+#cv2.waitKey(0)
+
+grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray", grey)
+cv2.waitKey(0)
+hist = cv2.calcHist([grey], [0], None, [256], [0, 256])
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+plt.show()
