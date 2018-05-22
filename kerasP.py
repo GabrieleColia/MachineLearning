9626089b9213c8c97d66e61a8f6c705a91589914
Gabriele <gabrielecolia@yahoo.it>
2018-05-22 13:01:38 +0200

    reti neurali keras

--- kerasP.py
+++ kerasP.py
@@ -23,23 +23,23 @@ classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metric
 #preprocessing per evitare overfitting
 train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2,zoom_range = 0.2,horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory('trainP',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
-test_set = test_datagen.flow_from_directory('testP',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
+training_set = train_datagen.flow_from_directory('dataset/images/lab',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
+test_set = test_datagen.flow_from_directory('test',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
 
-classifier.fit_generator(training_set,steps_per_epoch = 16,epochs = 10,validation_data = test_set,validation_steps = 40)
+classifier.fit_generator(training_set,steps_per_epoch = 281,epochs = 10,validation_data = test_set,validation_steps = 100)
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('v.jpg', target_size = (64, 64))
+test_image = image.load_img('alba.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 print(training_set.class_indices)
 if result[0][0] == 0:
-    prediction = 'armadillo'
+    prediction = 'albatross'
 elif result[0][0] == 1:
-    prediction = 'procione'
+    prediction = 'Fulmar'
 else:
-    prediction = 'volpe'
+    prediction = 'pellicano'
 
 print(prediction)
